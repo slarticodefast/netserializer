c6c9985e1c9ee49f102a2d5076666b4710c1a8e6
DrSmugleaf <drsmugleaf@gmail.com>
2025-07-23 01:24:08 -0700

    Add immutable dictionary and immutable hashset serializer

--- NetSerializer/Serializer.cs
+++ NetSerializer/Serializer.cs
@@ -8,12 +8,14 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Diagnostics;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using NetSerializer.TypeSerializers;
 
 namespace NetSerializer
@@ -34,6 +36,8 @@ namespace NetSerializer
 			new ListSerializer(),
 			new NetListAsArraySerializer(),
 			new HashSetSerializer(),
+			new ImmutableDictionarySerializer(),
+			new ImmutableHashSetSerializer(),
 			new GenericSerializer(),
 		};
 
@@ -65,7 +69,7 @@ namespace NetSerializer
 
 				AddTypesInternal(new Dictionary<Type, uint>()
 				{
-					{ typeof(object), Serializer.ObjectTypeId }
+					{ typeof(object), ObjectTypeId }
 				});
 
 				AddTypesInternal(rootTypes);
@@ -103,7 +107,7 @@ namespace NetSerializer
 
 				AddTypesInternal(new Dictionary<Type, uint>()
 				{
-					{ typeof(object), Serializer.ObjectTypeId }
+					{ typeof(object), ObjectTypeId }
 				});
 
 				AddTypesInternal(typeMap);
@@ -292,7 +296,7 @@ namespace NetSerializer
 
 			stream.Position = 0;
 
-			using var sha256 = System.Security.Cryptography.SHA256.Create();
+			using var sha256 = SHA256.Create();
 			var bytes = sha256.ComputeHash(stream);
 
 			return Convert.ToHexString(bytes);
@@ -316,7 +320,7 @@ namespace NetSerializer
 		[Conditional("DEBUG")]
 		void AssertLocked()
 		{
-			Debug.Assert(System.Threading.Monitor.IsEntered(m_modifyLock));
+			Debug.Assert(Monitor.IsEntered(m_modifyLock));
 		}
 
 		public void Serialize(Stream stream, object ob)
